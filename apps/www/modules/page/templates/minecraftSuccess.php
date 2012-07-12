a5f53542305789c5536351b8efc603460f10959a
Simon Vieille <simon.vieille@free.fr>
2012-07-12 10:07:12 +0200

    minecraft page

--- apps/www/modules/page/templates/minecraftSuccess.php
+++ apps/www/modules/page/templates/minecraftSuccess.php
@@ -1,6 +1,8 @@
 <p>La communauté <strong>Deblan</strong> dispose à présent de son serveur <strong>Minecraft</strong> dédié ! Pour rappel, <strong>Minecraft est un jeu indépendant</strong> développé par la société <a href="http://www.mojang.com/">Mojang</a>. L'objectif est simple puisqu'il n'y en a pas :) Vous avez la possibilité de construire le monde que vous voulez dans un style graphique simplifié mais ultra complet.</p>
 
+<hr />
 <p><img src="/images/mc.jpg" alt="Minecraft" title="Minecraft" /></p>
+<hr />
 
 <p>Pour venir jouer avec nous, connectez-vous sur <a>PLAY.DEBLAN.FR</a>.</p>
 
